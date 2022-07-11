cb9ac92a932fcb79f0e698e95e209f09e0b275e3
BrianH <brianhwang29@gmail.com>
2022-07-11 12:24:25 +0900

    modified

--- react-begin/src/App.js
+++ react-begin/src/App.js
@@ -1,36 +1,25 @@
-// import Button from './Button';
-import styles from './App.module.css'
 import { useState, useEffect } from 'react'
 
+function Hello(){
+  useEffect(() => {
+    console.log('created 😃');
+
+    //clean up function to do smthing when destroyed
+    return () => console.log('destroyed 😥')
+  }, [])
+  return <h1>hello</h1>
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState('');
-  const onChange = (event) => setKeyword(event.target.value); 
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
 
-  const onClick = () => setValue((prev) => prev + 1)
-  console.log('i run all the time');
-
-  useEffect(() => {
-    console.log('CALL THE API')
-  }, []);
-
-  useEffect(() => {
-    if(keyword !== '' && keyword.length > 5) {
-      console.log('SEARCH FOR...', keyword)
-    }
-  }, [keyword])
-
-  useEffect(() => {
-      console.log('i run when both changes')
-  }, [keyword, counter])
-
-  return (
-    <div>
-      <input value={keyword} type="text" onChange={onChange} placeholder="Seach here.." />
-      <h1 className={styles.title}>{counter}</h1>
-      <button onClick={onClick}>click me</button>
-    </div>
-  );
+ return (
+  <div>
+    {showing ? <Hello/> : null}
+    <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+  </div>
+ )
 }
 
 export default App;
