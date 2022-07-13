2d762f0445549872ff7cd11bb5843c4128349fd7
BrianH <brianhwang29@gmail.com>
2022-07-13 12:54:10 +0900

    movie.js

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,28 +1,37 @@
 import {useState, useEffect} from 'react'
+import Movie from './Movie'
 
 function App() {
-  const [loading, setLoading] = useState(true); 
-  const [coins, setCoins] = useState([])
-  useEffect(() => {
-    fetch('https://api.coinpaprika.com/v1/tickers')
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    const getMovies = async () => {
+        const res = await fetch(`https://yts.mx/api/v2/list_movies.jsonminimum_rating=8.8&sort_by=year`)
+        const json = await res.json();
         setLoading(false);
-      })
-  }, [])
-  
-  return (
-    <div>
-      <h1>The Coins! {loading ? '' : `(${coins.length})`}</h1>
-      { loading ? 
-        <strong>Loading...</strong> : 
-        <select>
-        {coins.map((item) => <option key={item.id}>{item.name} ({item.symbol}) : ${item.quotes['USD'].price}</option>)}
-      </select>  
-      }
-    </div>
-  )
+        setMovies(json.data.movies);
+
+    }
+    useEffect(() => {
+        getMovies();
+    },[])
+
+    return (
+        <div>
+            { loading ?
+                <h1>Loading...</h1> :
+                 <div>
+                    { movies.map((movie) => (
+                        <Movie
+                            key={movie.id}
+                            coverImg={movie.medium_cover_image}
+                            title={movie.title}
+                            summary={movie.summary}
+                        />
+                    ))},
+                </div>
+            }
+        </div>
+    )
 }
 
 export default App;
